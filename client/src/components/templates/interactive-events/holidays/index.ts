a659a433dbc6543121eb1dbb20035f6d25608d48
ilayadmoni <ilayadmoni9@gmail.com>
2026-05-16 22:46:21 +0300

    feat(interactive-events): add barrel export for holidays

---
+++ client/src/components/templates/interactive-events/holidays/index.ts
@@ -0,0 +1,9 @@
+export {
+  HolidayRoshHashanahInteractive,
+  HolidayPassoverInteractive,
+  HolidayPurimInteractive,
+  HolidayShavuotInteractive,
+  HolidaySukkotInteractive,
+  HolidayHanukkahInteractive,
+} from "./HolidayWrappers";
+export { HOLIDAY_INTERACTIVE_CONFIGS, HOLIDAY_INTERACTIVE_SLUGS } from "./holiday-config";
