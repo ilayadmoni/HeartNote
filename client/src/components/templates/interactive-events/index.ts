8333db0b043f6bd518b7f35de02605a2cd7387ca
ilayadmoni <ilayadmoni9@gmail.com>
2026-05-16 22:47:27 +0300

    feat(interactive-events): add root barrel export

---
+++ client/src/components/templates/interactive-events/index.ts
@@ -0,0 +1,18 @@
+export { BirthdayCandlesInteractive } from "./birthday";
+export { WeddingGlassInteractive } from "./wedding";
+export {
+  HolidayRoshHashanahInteractive,
+  HolidayPassoverInteractive,
+  HolidayPurimInteractive,
+  HolidayShavuotInteractive,
+  HolidaySukkotInteractive,
+  HolidayHanukkahInteractive,
+} from "./holidays";
+export type {
+  InteractiveGreetingData,
+  BirthdayInteractiveData,
+  WeddingInteractiveData,
+  HolidayInteractiveSlug,
+  HolidayInteraction,
+  HolidayInteractiveConfig,
+} from "./types";
